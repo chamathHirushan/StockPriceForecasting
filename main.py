8d43301fc6bffbf4896a454ba0c82b677d5192c8
chamathHirushan <chamathg.21@cse.mrt.ac.lk>
2024-09-24 06:55:45 +0530

    data fetching and displaying

--- main.py
+++ main.py
@@ -6,8 +6,6 @@ import yfinance as yf
 from datetime import date
 import streamlit as st
 
-print("hello world")
-
 
 START = "2018-01-01"
 TODAY = date.today().strftime("%Y-%m-%d")
@@ -15,8 +13,23 @@ TODAY = date.today().strftime("%Y-%m-%d")
 st.title("Stock Prediction App")
 
 stocks = ("AAPL", "GOOG", "MSFT")
-selected_stocks = st.selectbox("Select the dataset for predictions", stocks)
+selected_stock = st.selectbox("Select the dataset for predictions", stocks)
 
 n_years = st.slider("Years of prediction :", 1, 4)
 
 period = n_years * 365
+
+
+@st.cache_data  # cache the data
+def load_data(stock_name):
+    data = yf.download(stock_name, START, TODAY)
+    data.reset_index(inplace=True)
+    return data
+
+
+data_load_state = st.text("Load data for " + selected_stock + "...")
+data = load_data(selected_stock)
+data_load_state = st.text("loaded " + selected_stock + " successfully")
+
+st.subheader("Raw data")
+st.write(data.tail())
